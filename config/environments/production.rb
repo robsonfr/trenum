ae95d93d5aa3e7b266031a05cacffb91fed48261
Raphael Caldas <rapcal@gmail.com>
2012-02-02 18:54:00 -0200

    Trying to fix deep symbolize error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Trenum::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { :host => 'trenum.heroku.com' }
   # Enable threaded mode
   # config.threadsafe!
 
